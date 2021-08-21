94176c78d8c9660cfa998b4a3832fa3faa0c445d
Horie <17owlet@gmail.com>
2021-08-22 00:50:41 +0900

    変更した内容をメモ

---
+++ anime.js
@@ -0,0 +1,17 @@
+$(function(){
+  
+    var cursor=$("#cursor");
+    
+
+    $(document).on("mousemove",function(e){
+
+      var x=e.clientX;
+      var y=e.clientY;
+
+      cursor.css({
+        "opacity":"1",
+        "top":y+"px",
+        "left":x+"px"
+      });
+    });
+  });
