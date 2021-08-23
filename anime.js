784f0597bd546442eb826b9cc3dd097305f4bc5a
Horie <17owlet@gmail.com>
2021-08-23 14:07:23 +0900

    変更した内容をメモ

--- anime.js
+++ anime.js
@@ -17,10 +17,19 @@ $(function(){
     });
     //ここまでのカーソル変更
     
-    //ローディング画面
+
+    
+    //ローディング画面案1
+    // $(window).on('load',function(){
+    //   $("#loading").fadeOut('slow');
+    //   $("#loading_word").fadeOut('slow');
+    // })
+
+    //ローディング画面案2
     $(window).on('load',function(){
-      $("#loading").fadeOut('slow');
-      $("#loading_word").fadeOut('slow');
+      $("#loading").slideUp('slow');
+      $("#loading2").delay(100).slideUp('slow');
+      $("#loading3").delay(200).slideUp('slow');
     })
 
   });
